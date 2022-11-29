1a54f88e7619bc5e025a4053c2c8dc64df9a6bd1
Andrearusso94 <andrea.r.4845@gmail.com>
2022-11-29 14:11:46 +0100

    initial commit and create array characters

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+/*Milestone 1
+Creare un form che invii in GET la lunghezza della password. 
+Una nostra funzione utilizzerà questo dato per generare una password casuale 
+(composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente. 
+Scriviamo tutto (logica e layout) in un unico file index.php
+*/
+$psCharacters = ["a", "b", "c", "d", "e", "f", "A", "B", "C", "D", "E", "F", 1, 2, 3, 4, 5, 6, "!", "?", "$", "%",];
+
+var_dump($psCharacters);
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <header>
+        <!-- place navbar here -->
+    </header>
+    <main>
+        <div class="mb-3">
+            <form action="index.php"> <label for="" class="form-label">Password</label>
+                <input type="text" class="form-control" name="password" id="password" aria-describedby="helpId" placeholder="">
+                <small id="helpId" class="form-text text-muted">lunghezza password</small>
+            </form>
+
+        </div>
+        <button type="submit">Invia</button>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js" integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
